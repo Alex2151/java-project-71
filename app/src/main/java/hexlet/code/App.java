b452a774e082612fbff9c7bbb5f7a6fae17bdc09
Alex Spiridonow <spiridonow.sania@yandex.ru>
2024-12-18 16:45:48 +0300

    step4

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -14,22 +14,44 @@ import java.nio.file.Files;
 import java.security.MessageDigest;
 import java.util.concurrent.Callable;
 
-@Command(name = "checksum", mixinStandardHelpOptions = true, version = "checksum 4.0",
-         description = "Prints the checksum (SHA-256 by default) of a file to STDOUT.")
+@Command(name = "gendiff", mixinStandardHelpOptions = true,
+		 version = "gendiff 1.0",
+         description = "Compares two configuration files and shows a difference.")
 class App implements Callable<Integer> {
-
-    @Parameters(index = "0", description = "The file whose checksum to calculate.")
+	//
+	/*
+    @Parameters(index = "0",
+				description = "The file whose checksum to calculate.")
     private File file;
-
-    @Option(names = {"-a", "--algorithm"}, description = "MD5, SHA-1, SHA-256, ...")
+	*/
+	@Parameters(index = "0",
+				description = "path to first file",
+			    paramLabel = "filepath1")
+    private File file1;
+	
+	@Parameters(index = "1",
+				description = "path to second file",
+			    paramLabel = "filepath2"/*,
+			   	idden = true*/)
+    private File file2;
+	/*
+    @Option(names = {"-a", "--algorithm"},
+					 description = "MD5, SHA-1, SHA-256, ...")
     private String algorithm = "SHA-256";
-
+	*/
+	@Option(names = {"-f", "--format"},
+			 description = "output format [default: stylish]",
+		   paramLabel = "format")
+	private String format;
+	
     @Override
     public Integer call() throws Exception { // your business logic goes here...
-        byte[] fileContents = Files.readAllBytes(file.toPath());
+        /*
+		byte[] fileContents = Files.readAllBytes(file.toPath());
         byte[] digest = MessageDigest.getInstance(algorithm).digest(fileContents);
         System.out.printf("%0" + (digest.length*2) + "x%n", new BigInteger(1, digest));
-        return 0;
+        */
+		return 0;
     }
 
     // this example implements Callable, so parsing, error handling and handling user
